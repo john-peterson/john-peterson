f73cf7ad7b1f41f119645ba758847708fac26c0d
John Peterson <john.peterson3@hotmail.com>
2013-01-27 16:27:31 +0100

    Updates to csumtree, sfcex

---
+++ common/common.cs
@@ -0,0 +1,155 @@
+﻿// © John Peterson. License GNU GPL 3.
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace Shared
+{
+public class Common
+{
+	public static String logFile;
+
+	public static void dbgMsg(String s, bool newLine = true) {
+#if (DEBUG)
+		Msg(s);
+#endif
+	}
+	public static void errMsg(String s, bool newLine = true) {
+		Msg(s, ConsoleColor.Red);
+	}
+
+	public static void Msg(String s, ConsoleColor c = ConsoleColor.Gray, bool newLine = true) {
+		if (newLine) s = s + "\n";
+		Console.ForegroundColor = c;
+		Console.Write(s);
+		Console.ResetColor();
+		fileMsg(s);
+	}
+	public static void Msg(String s, bool newLine = true) {
+		Msg(s, ConsoleColor.Gray, newLine);
+	}
+	public static void Msg(String s) {
+		Msg(s, ConsoleColor.Gray, true);
+	}
+
+	public static void fileMsg(String s) {
+		if (String.IsNullOrEmpty(logFile)) return;
+		StreamWriter sw = File.AppendText(logFile);
+		sw.Write(s);
+		sw.Flush();
+		sw.Close();
+	}
+
+	// files
+
+	// take ownership and set write permission
+	public static void takeOwn(String file) {
+		String perm;
+		Process p = new Process();
+		p.StartInfo.FileName = "takeown";
+		p.StartInfo.Arguments = String.Format("/f \"{0}\"", file);
+		p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+		p.Start();
+		p.WaitForExit();
+		dbgMsg(String.Format("{0} {1}", p.StartInfo.FileName, p.StartInfo.Arguments));
+		p.StartInfo.FileName = "icacls";
+		if (Common.IsDir(file))
+			perm = "(oi)(ci)f";
+		else
+			perm = "f";
+		p.StartInfo.Arguments = String.Format("\"{0}\" /grant administrators:{1}", file, perm);
+		p.Start();
+		p.WaitForExit();
+		dbgMsg(String.Format("{0} {1}", p.StartInfo.FileName, p.StartInfo.Arguments));
+	}
+
+	// copy
+	public static void copy(String src, String dst, bool sourceTakeown = false, bool ignoreError = false) {
+		// create target path
+		String dstDir = Path.GetDirectoryName(dst);
+		if (String.IsNullOrEmpty(dstDir)) return;
+
+		if (!String.IsNullOrEmpty(dstDir) && !Directory.Exists(dstDir))
+			createDir(dstDir);
+
+		try {
+			if (sourceTakeown) takeOwn(src);
+			takeOwn(dstDir);
+			takeOwn(dst);
+			Msg("► copy /y ", false); Msg(String.Format("{0}", src), ConsoleColor.Green, false); Msg(String.Format(" {0}", dst));
+			System.IO.File.Copy(src, dst, true);
+		} catch (Exception e) {
+			errMsg(e.Message);
+			if (!ignoreError) Environment.Exit(0);
+		}
+	}
+
+	// create directory
+	public static void createDir(String path) {
+		int r = 0;
+		createDir_(path, ref r);
+	}
+	public static void createDir_(String path, ref int r) {
+		try {
+			dbgMsg(String.Format("► mkdir \"{0}\"", path));
+			Directory.CreateDirectory(path);
+			takeOwn(path);
+		} catch (Exception e) {
+			errMsg(e.Message);
+			if (r > 0) {
+				dbgMsg(String.Format("Can't create {0}", path));
+				Environment.Exit(0);
+				return;
+			}
+			takeOwn(Path.GetDirectoryName(path));
+			r++;
+			createDir_(path, ref r);
+			return;
+		}
+	}
+
+	// extract wim file
+	public static void extractFile(String src, String relPath, String dSrc, int ed) {
+		if (!Common.Exists("7z.exe")) {
+			Console.WriteLine("\n\"7z.exe\" missing");
+			Environment.Exit(0);
+		}
+		Process p = new Process();
+		p.StartInfo.FileName = "7z";
+		p.StartInfo.Arguments = String.Format("e -aoa {0}:\\sources\\install.wim {1}{2}", dSrc.ToUpper(), ed, relPath);
+		p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+		p.Start();
+		// should be called after start
+		p.PriorityClass = ProcessPriorityClass.Idle;
+		p.WaitForExit();
+		Common.dbgMsg(String.Format("{0} {1}", p.StartInfo.FileName, p.StartInfo.Arguments));
+	}
+
+	public static bool Exists(string fileName) {
+		if (GetFullPath(fileName) != null)
+			return true;
+		return false;
+	}
+
+	public static string GetFullPath(string fileName) {
+		if (File.Exists(fileName))
+			return Path.GetFullPath(fileName);
+
+		var values = Environment.GetEnvironmentVariable("PATH");
+		foreach (var path in values.Split(';')) {
+			var fullPath = Path.Combine(path, fileName);
+			if (File.Exists(fullPath))
+				return fullPath;
+		}
+		return null;
+	}
+
+	public static bool IsDir(String path) {
+		return (File.GetAttributes(path) & FileAttributes.Directory) == FileAttributes.Directory;
+	}
+}
+}
--- csumtree/csumtree.cs
+++ csumtree/csumtree.cs
@@ -1,5 +1,4 @@
-﻿// System File Checker (sfc) log scan and copy utility
-// (C) John Peterson, GNU GPL 3
+// © John Peterson. License GNU GPL 3.
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -8,210 +7,165 @@ using System.Security.Principal;
 using System.Security.AccessControl;
 using System.Security.Cryptography;
 using System.Text;
+using Shared;
 
 namespace csumtree {
-	class csumtree {
-		static String src = "", dst = "";
-		static bool bFix = false;
+class csumtree {
+	static String src = "", dst = "";
+	static bool copyMissing = false, copyDifferent = false, ignoreReadError = false, ignoreWriteError = false;
 
-		// usage
-		private static void usage() {
-			String str = "\nRecursive folder checksum comparison.\n" +
-							"(C) John Peterson 2012, GNU GPL 3\n\n" +
-							"csumtree source destination [/f]\n\n" +
-							"/f\tReplace mismatched files. Otherwise list changes only.";
-			Console.WriteLine(str);
-		}
+	// usage
+	private static void usage() {
+		String str = "\nRecursive folder checksum comparison.\n" +
+						"© John Peterson. License GNU GPL 3.\n\n" +
+						"csumtree source destination [/f]\n\n" +
+						"/m\tReplace missing files\n" +
+						"/f\tReplace mismatched files\n" +
+						"/ir\tIgnore read error\n" +
+						"/iw\tIgnore write error\n";
+		Console.WriteLine(str);
+	}
 
-		static void dbgMsg(String s, bool newLine = true) {
-			if (newLine) s = s + "\n";
-#if (DEBUG)
-			Console.Write(s);
-#endif
-			fileMsg(s);
-		}
-		static void errMsg(String s, bool newLine = true) {
-			if (newLine) s = s + "\n";
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.Write(s);
-			Console.ResetColor();
-			fileMsg(s);
-		}
+	// files compared
+	static bool isSystemFile(String file) {
+		String ext = Path.GetExtension(file);
+		if (ext == ".sys"
+			|| ext == ".dll"
+			|| ext == ".exe"
+			|| ext == ".mum"
+			|| ext == ".cat"
+			|| ext == ".gpd"
+			|| ext == ".xml"
+			|| ext == ".ttf") return true;
+		else
+			return false;
+	}
 
-		static void Msg(String s, ConsoleColor c = ConsoleColor.Gray, bool newLine = true) {
-			if (newLine) s = s + "\n";
-			Console.ForegroundColor = c;
-			Console.Write(s);
-			Console.ResetColor();
-			fileMsg(s);
-		}
-		
-		static void fileMsg(String s) {
-			StreamWriter sw = File.AppendText("csumtree.log");
-			sw.Write(s);
-			sw.Flush();
-			sw.Close();
-		}
+	static void Main(string[] args) {
+		readArg(args);
 
-		static bool isSystemFile(String file) {
-			String ext = Path.GetExtension(file);
-			if (ext == ".sys"
-				|| ext == ".dll"
-				|| ext == ".exe"
-				|| ext == ".mum"
-				|| ext == ".cat"
-				|| ext == ".ttf") return true;
-			else
-				return false;
-		}
+		// reset log file
+		FileStream log = new FileStream("csumtree.log", FileMode.Create);
+		log.Close();
+		Common.logFile = "csumtree.log";
 
-		static List<String> GetFilesEx(string folder, ref List<String> files) {
-			foreach (string file in Directory.GetFiles(folder)) {
-				files.Add(file);
-			}
-			foreach (string subDir in Directory.GetDirectories(folder)) {
-				try {
-					GetFilesEx(subDir, ref files);
-				} catch (Exception e) {
-					errMsg(e.Message);
-				}
-			}
-			return files;
-		}
+		// check files
+		CheckFiles(src);
 
-		// take ownership and set write permission
-		public static void TakeOwn(String file) {
-			dbgMsg(String.Format("Taking ownership of {0}", file));
-			Process p = new Process();
-			p.StartInfo.FileName = "takeown";
-			p.StartInfo.Arguments = String.Format("/f \"{0}\"", file);
-			p.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
-			p.Start();
-			p.WaitForExit();
-			p.StartInfo.FileName = "icacls";
-			p.StartInfo.Arguments = String.Format("\"{0}\" /grant administrators:F", file);
-			p.Start();
-			p.WaitForExit();
-		}
+		Common.Msg("Done");
+	}
 
-		// copy
-		static void copy(String src, String dst, bool sourceTakeown = false) {
-			Msg(String.Format("►Copy {0} to {1}", src, dst), bFix ? ConsoleColor.Green : ConsoleColor.Yellow);
-			if (!bFix) return;
+	// create list of files in path
+	static void CheckFiles(string folder) {
+		String srcPath, dstPath;
+		bool dstExist = false, bSame;
+
+		foreach (string file in Directory.GetFiles(folder)) {
+			srcPath = file;
+
+			// skip non system files
+			if (!isSystemFile(srcPath)) continue;
+
+			bSame = true;
+			dstPath = dst + srcPath.Substring(src.Length);
+
 			try {
-				if (sourceTakeown) TakeOwn(src);
-				TakeOwn(dst);
-				System.IO.File.Copy(src, dst, true);
+				dstExist = File.Exists(dstPath);
 			} catch (Exception e) {
-				dbgMsg(e.Message);
+				Common.errMsg(e.Message);
+				Common.takeOwn(dstPath);
+			}
+
+			// compare files
+			if (dstExist) {
+				String srcCS = getCS(srcPath);
+				String dstCS = getCS(dstPath);
+				if (srcCS.Length > 0 && dstCS.Length > 0) bSame = srcCS == dstCS;
+			}
+			// destination file is missing
+			if (!dstExist) {
+				Common.Msg("► ", false); Common.Msg(String.Format("{0}", dstPath), ConsoleColor.Yellow, false);
+				Common.Msg(String.Format(" missing [present at {0}]", srcPath), ConsoleColor.Gray);
+				if (copyMissing)
+					Common.copy(srcPath, dstPath, ignoreWriteError);
+			}
+			// file is different
+			if (dstExist && !bSame) {
+				Common.Msg("► ", false); Common.Msg(String.Format("{0}", dstPath), ConsoleColor.Red, false);
+				Common.Msg(String.Format(" mismatch [{0}]", srcPath), ConsoleColor.Gray);
+				if (copyDifferent)
+					Common.copy(srcPath, dstPath, ignoreWriteError);
 			}
 		}
 
-		static String parseArg(String arg) {
-			if (arg.IndexOf(":") == -1) {
-				Console.WriteLine("\nDirectory in \"{0}\" missing", arg);
-				Environment.Exit(0);
-			}
-			return arg.Substring(arg.IndexOf(":") + 1, 1);
-		}
-
-		static void readArg(string[] args) {
-			if (args.Length == 0) {
-				usage();
-				Environment.Exit(0);
-			}
-			if (args[0] == "/?") {
-				usage();
-				Environment.Exit(0);
-			}
-			List<String> largs = new List<String>();
-			for (int i = 0; i < args.Length; i++) {
-				if (args[i].Equals("/f", StringComparison.CurrentCultureIgnoreCase)) bFix = true;
-				if (!args[i][0].Equals('/')) largs.Add(args[i]);
-			}
-			if (largs.Count > 0) src = largs[0];
-			if (largs.Count > 1) dst = largs[1];
-			if (!Directory.Exists(src)) {
-				Console.WriteLine("\nSource \"{0}\" missing", src);
-				Environment.Exit(0);
-			}
-			if (!Directory.Exists(dst)) {
-				Console.WriteLine("\nDestination \"{0}\" missing", dst);
-				Environment.Exit(0);
-			}
-		}
-
-		static String getCSEx2(String file, ref int i) {
+		// recurse
+		foreach (string subDir in Directory.GetDirectories(folder)) {
 			try {
-				return getCS(file);
+				CheckFiles(subDir);
 			} catch (Exception e) {
-				if (i > 0) {
-					dbgMsg(String.Format("Cannot read checksum for {0}", file));
-					return "";
-				}
-				dbgMsg(e.Message);
-				TakeOwn(file);
-				i++;
-				getCSEx2(file, ref i);
-				return "";
+				Common.errMsg(e.Message);
 			}
 		}
+	}
 
-		static String getCSEx(String file) {
-			int i = 0;
-			return getCSEx2(file, ref i);
+	static void readArg(string[] args) {
+		if (args.Length == 0) {
+			usage();
+			Environment.Exit(0);
 		}
+		if (args[0] == "/?") {
+			usage();
+			Environment.Exit(0);
+		}
+		List<String> largs = new List<String>();
+		for (int i = 0; i < args.Length; i++) {
+			if (args[i].Equals("/m", StringComparison.CurrentCultureIgnoreCase)) copyMissing = true;
+			if (args[i].Equals("/d", StringComparison.CurrentCultureIgnoreCase)) copyDifferent = true;
+			if (args[i].Equals("/ir", StringComparison.CurrentCultureIgnoreCase)) ignoreReadError = true;
+			if (args[i].Equals("/iw", StringComparison.CurrentCultureIgnoreCase)) ignoreWriteError = true;
+			if (!args[i][0].Equals('/')) largs.Add(args[i]);
+		}
+		if (largs.Count > 0) src = largs[0];
+		if (largs.Count > 1) dst = largs[1];
 
-		static String getCS(String file) {
-			FileStream fs = new FileStream(file, FileMode.Open);
+		// exceptions
+		if (!Directory.Exists(src)) {
+			Console.WriteLine("\nSource \"{0}\" missing", src);
+			Environment.Exit(0);
+		}
+		if (!Directory.Exists(dst)) {
+			Console.WriteLine("\nDestination \"{0}\" missing", dst);
+			Environment.Exit(0);
+		}
+	}
+
+	// checksum
+	static String getCS(String file) {
+		int r = 0;
+		return getCS_(file, ref r);
+	}
+	static String getCS_(String file, ref int r) {
+		try {
+			FileStream fs = new FileStream(file, FileMode.Open, FileAccess.Read);
 			MD5 md5 = new MD5CryptoServiceProvider();
 			byte[] retVal = md5.ComputeHash(fs);
 			fs.Close();
 			StringBuilder sb = new StringBuilder();
-			for (int i = 0; i < retVal.Length; i++) {
+			for (int i = 0; i < retVal.Length; i++)
 				sb.Append(retVal[i].ToString("x2"));
-			}
 			return sb.ToString();
-		}
-
-		static void Main(string[] args) {
-			String file, dstPath;
-			bool bExist = false, bSame;
-			readArg(args);
-			List<String> files = new List<String>();
-			GetFilesEx(src, ref files);
-			FileStream log = new FileStream("csumtree.log", FileMode.Create);
-			log.Close();
-			for (int i = 0; i < files.Count; i++) {
-				String srcPath = files[i];
-				fileMsg(String.Format("{0}/{1} {2}\n", i, files.Count, srcPath));
-				if (!isSystemFile(srcPath)) continue;
-				bSame = true;
-				dstPath = dst + srcPath.Substring(src.Length);
-				file = Path.GetFileName(srcPath);
-				try {
-					bExist = File.Exists(dstPath);
-				} catch (Exception e) {
-					dbgMsg(e.Message);
-					TakeOwn(dstPath);
-				}
-				if (bExist) {
-					String srcCS = getCSEx(srcPath);
-					String dstCS = getCSEx(dstPath);
-					if (srcCS.Length > 0 && dstCS.Length > 0) bSame = srcCS == dstCS;
-				}
-				if (!bExist) {
-					Msg(String.Format("{0}", file), ConsoleColor.Yellow, false);
-					Msg(String.Format(" missing [{0}]", dstPath), ConsoleColor.Gray);
-				}
-				if (bExist && !bSame) {
-					Msg(String.Format("{0}", file), ConsoleColor.Red, false);
-					Msg(String.Format(" mismatch [{0}]", srcPath), ConsoleColor.Gray);
-					copy(srcPath, dstPath);
-				}
+		} catch (Exception e) {
+			Common.errMsg(e.Message);
+			if (r > 0) {
+				Common.dbgMsg(String.Format("Can't read checksum for {0}", file));
+				if (!ignoreReadError) Environment.Exit(0);
+				return "";
 			}
-			Console.WriteLine("Done. Press a key to continue . . .");
-			Console.ReadKey();
+			Common.takeOwn(file);
+			r++;
+			getCS_(file, ref r);
+			return "";
 		}
 	}
+}
 }
